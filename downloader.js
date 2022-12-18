f87a597fcb438a45361119b1f94b682bd363bf1b
HeppaTytot <120368632+HeppaTytot@users.noreply.github.com>
2022-12-18 12:37:58 +0200

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -2,12 +2,15 @@ const date = new Date().getTime();
 const countDate = new Date(1671667200000).getTime();
 function DownloadAndroid(){
     if(date < countDate){
-        alert("Game not yet available");
+        alert("Peli ei vielä saatavilla");
+    }
+    else{
+        window.open('resources/games/Garge.apk')
     }
 }
 function DownloadWindows(){
     if(date < countDate){
-        alert("Game not yet available");
+        alert("Peli ei vielä saatavilla");
     }
     else{
         window.open('resources/games/Garge.exe')
